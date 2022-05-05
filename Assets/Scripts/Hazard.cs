ef10433a7851de759824ffbb193445c3206f9830
Eduardo Almeida <contato.eduardopex@hotmail.com>
2022-05-05 19:04:46 -0300

    chore: updated hazard rules.

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+  Vector3 rotation;
+
+  private void Start()
+  {
+    var xRotation = Random.Range(0.2f, 1f);
+    rotation = new Vector3(1, 0);
+  }
+
+  private void Update()
+  {
+    transform.Rotate(rotation);
+  }
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (!collision.gameObject.CompareTag("Hazard"))
     {
-        Destroy(gameObject);
+      Destroy(gameObject);
     }
+
+  }
 }
